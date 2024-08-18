5fc1d57afb493d588a32518c46e2b66ca817a444
James Jesudason <james.jesudason@gmail.com>
2024-08-18 21:04:07 +0100

    [feat] Install fugitive and git-signs

--- init.lua
+++ init.lua
@@ -782,14 +782,17 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   'maxmx03/solarized.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = 'light'
+  --     vim.cmd.colorscheme 'solarized'
+  --   end,
+  -- },
   {
-    'maxmx03/solarized.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.o.background = 'light'
-      vim.cmd.colorscheme 'solarized'
-    end,
+    'tpope/vim-fugitive',
   },
 
   { -- You can easily change to a different colorscheme.
@@ -894,8 +897,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
